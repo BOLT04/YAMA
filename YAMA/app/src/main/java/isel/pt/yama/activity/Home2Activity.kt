42b1082d2a11827c7489c86564f6cf85c31787e9
David <didicp4@gmail.com>
2018-12-02 12:35:12 +0000

    finished Home with options toolbar; needs testing

--- YAMA/app/src/main/java/isel/pt/yama/activity/Home2Activity.kt
+++ YAMA/app/src/main/java/isel/pt/yama/activity/Home2Activity.kt
@@ -1,11 +1,14 @@
 package isel.pt.yama.activity
 
+import android.content.Context
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import isel.pt.yama.R
-
+import isel.pt.yama.common.SP_NAME
 import kotlinx.android.synthetic.main.activity_home2.*
+
 
 class Home2Activity : AppCompatActivity() {
 
@@ -13,7 +16,19 @@ class Home2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home2)
         setSupportActionBar(toolbar)
-
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) =
+        when (item?.itemId) {
+            R.id.logout -> {
+                getSharedPreferences(SP_NAME, Context.MODE_PRIVATE).edit().clear().apply()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
