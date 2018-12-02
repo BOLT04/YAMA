559f210ad695bb7c9791aaf27408f32eb94ece7a
Farinha9 <9farinha9@gmail.com>
2018-12-02 02:48:17 +0000

    logout implementation started

--- YAMA/app/src/main/java/isel/pt/yama/activity/Home2Activity.kt
+++ YAMA/app/src/main/java/isel/pt/yama/activity/Home2Activity.kt
@@ -14,10 +14,6 @@ class Home2Activity : AppCompatActivity() {
         setContentView(R.layout.activity_home2)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
 }
